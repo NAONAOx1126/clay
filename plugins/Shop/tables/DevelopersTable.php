cc0f5f46a1426f7f5192100337b4735463edecfd
Naohisa Minagawa <info@sweetberry.jp>
2011-12-04 02:35:31 +0900

    管理ユーザー系のテーブルを差し替え。 それに伴い、製造元、販売元のショップ用のテーブルを法人ユーザーテーブルに統合

--- plugins/Shop/tables/DevelopersTable.php
+++
@@ -1,8 +0,0 @@
-<?php
-class Shop_DevelopersTable extends DatabaseTable{
-	function __construct(){
-		$this->db = DBFactory::getConnection("shop");
-		parent::__construct("shop_developers", "shop");
-	}
-}
-?>
--- plugins/Shop/tables/SellersTable.php
+++
@@ -1,8 +0,0 @@
-<?php
-class Shop_SellersTable extends DatabaseTable{
-	function __construct(){
-		$this->db = DBFactory::getConnection("shop");
-		parent::__construct("shop_sellers", "shop");
-	}
-}
-?>
