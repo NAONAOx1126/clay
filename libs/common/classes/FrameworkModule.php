80dbee10967f4135268504092436e14f400ed441
Naohisa Minagawa <info@clay-system.jp>
2012-09-06 19:04:37 +0900

    モジュールベースに一覧表示用のベースモジュールを追加 メール送信処理を見直し

--- libs/common/classes/FrameworkModule.php
+++ libs/common/classes/FrameworkModule.php
@@ -31,4 +31,145 @@ abstract class FrameworkModule{
 		exit;
 	}
 }
+
+/**
+ * 一覧取得用のモジュールクラスになります。
+ */
+abstract class FrameworkListModule extends FrameworkModule{
+	abstract protected function getModelName();
+	
+	abstract protected function getDefaultResultKey();
+
+	protected function getLoader(){
+		return new PluginLoader();
+	}
+	
+	protected function getExtendedCondition($condition, $params){
+		return $condition;
+	}
+	
+	protected function getSearchKeys(){
+		return array();
+	}
+	
+	protected function getColumnByKey($key){
+		return $key;
+	}
+	
+	protected function getDefaultByKey($key){
+		return null;
+	}
+	
+	public function execute($params){
+		// 検索に使用するプラグインローダーを取得する。
+		$loader = $this->getLoader();
+		$loader->LoadSetting();
+
+		// 検索条件を構築する。
+		$conditions = array();
+		
+		// 拡張の検索条件を設定する。
+		$condition = $this->getExtendedCondition($condition, $params);
+		
+		// タグのパラメータから検索条件を抽出
+		$keys = $this->getSearchKeys();
+		foreach($keys as $key){
+			if($this->getDefaultByKey($key) != null || $params->check($key)){
+				$condition[$this->getColumnByKey($key)] = $params->get($key, $this->getDefaultByKey($key));
+			}
+		}
+		
+		// HTTPのリクエストデータから検索条件を抽出
+		foreach($keys as $key){
+			if(isset($_POST["search"][$key])){
+				$condition[$this->getColumnByKey($key)] = $_POST["search"][$key];
+			}
+		}
+		
+		// 並べ替え順序が指定されている場合に適用
+		$sortOrder = "";
+		$sortReverse = false;
+		if($params->check("_sort_key")){
+			$sortOrder = $params->get("_sort_key");
+		}
+		if(isset($_POST["_sort_key"])){
+			$sortOrder = $_POST["_sort_key"];
+		}
+		if(empty($sortOrder)){
+			$sortOrder = "create_time";
+			$sortReverse = true;
+		}elseif(preg_match("/^rev@/", $sortOrder) > 0){
+			list($dummy, $sortOrder) = explode("@", $sortOrder);
+			$sortReverse = true;
+		}
+		
+		// 商品データを検索する。
+		$model = $loader->LoadModel($this->getModelName());
+		$result = $model->findAllBy($conditions, $sortOrder, $sortReverse);
+		
+		$_SERVER["ATTRIBUTES"][$params->get("_result", $this->getDefaultResultKey())] = $result;
+	}
+}
+
+/**
+ * ページング一覧表示用のモジュールです。
+ */
+abstract class FrameworkPageModule extends FrameworkListModule{
+	public function execute($params){
+		// 検索に使用するプラグインローダーを取得する。
+		$loader = $this->getLoader();
+		$loader->LoadSetting();
+
+		// ページャの初期化
+		$pager = new TemplatePager($params->get("_pager_mode", TemplatePager::PAGE_SLIDE), $params->get("_pager_dispmode", TemplatePager::DISPLAY_ATTR), $params->get("_pager_per_page", 20), $params->get("_pager_displays", 3));
+		$pager->importTemplates($params);
+		
+		// 検索条件を構築する。
+		$conditions = array();
+		
+		// 拡張の検索条件を設定する。
+		$condition = $this->getExtendedCondition($condition, $params);
+		
+		// タグのパラメータから検索条件を抽出
+		$keys = $this->getSearchKeys();
+		foreach($keys as $key){
+			if($this->getDefaultByKey($key) != null || $params->check($key)){
+				$condition[$this->getColumnByKey($key)] = $params->get($key, $this->getDefaultByKey($key));
+			}
+		}
+		
+		// HTTPのリクエストデータから検索条件を抽出
+		foreach($keys as $key){
+			if(isset($_POST["search"][$key])){
+				$condition[$this->getColumnByKey($key)] = $_POST["search"][$key];
+			}
+		}
+		
+		// 並べ替え順序が指定されている場合に適用
+		$sortOrder = "";
+		$sortReverse = false;
+		if($params->check("_sort_key")){
+			$sortOrder = $params->get("_sort_key");
+		}
+		if(isset($_POST["_sort_key"])){
+			$sortOrder = $_POST["_sort_key"];
+		}
+		if(empty($sortOrder)){
+			$sortOrder = "create_time";
+			$sortReverse = true;
+		}elseif(preg_match("/^rev@/", $sortOrder) > 0){
+			list($dummy, $sortOrder) = explode("@", $sortOrder);
+			$sortReverse = true;
+		}
+		
+		// 商品データを検索する。
+		$model = $loader->LoadModel($this->getModelName());
+		$pager->setDataSize($model->countBy($conditions));
+		$model->limit($pager->getPageSize(), $pager->getCurrentFirstOffset());
+		$result = $model->findAllBy($conditions, $sortOrder, $sortReverse);
+		
+		$_SERVER["ATTRIBUTES"][$params->get("_result", $this->getDefaultResultKey())."_pager"] = $pager;
+		$_SERVER["ATTRIBUTES"][$params->get("_result", $this->getDefaultResultKey())] = $result;
+	}
+}
 ?>
--- libs/common/classes/SendMail.php
+++ libs/common/classes/SendMail.php
@@ -45,6 +45,21 @@ class SendMail{
 	 * メールの本文
 	 */
 	protected $body;
+	
+	/**
+	 * メールの追加本文
+	 */
+	protected $extBody;
+	
+	/**
+	 * 添付ファイル
+	 */
+	protected $files;
+
+	/**
+	 * インライン画像ファイル
+	 */
+	protected $images;
 
     /**
 	 * コンストラクタです。テキストメールのための初期設定を行います。
@@ -54,6 +69,8 @@ class SendMail{
 	public function __construct(){
 		$this->db = null;
 		$this->body ="";
+		$this->extBody = array();
+		$this->files = array();
 	}
 	
     /**
@@ -107,6 +124,42 @@ class SendMail{
 	public function addBody($parts){
 		$this->body .= $parts;
 	}
+
+    /**
+	 * メールの追加本文を設定します。
+	 *
+	 * @params string $data メールのファイルデータ
+	 * @params string $mimeType メールのファイルデータのMIMEタイプ
+	 * @params string $encoding メールのファイルデータのエンコード
+     * @access public
+     */
+	public function setExtBody($data, $mimeType = "text/plain", $encoding = "7bit"){
+		$this->extBody = array("data" => $data, "mimeType" => $mimeType, "encoding" => $encoding);
+	}
+
+    /**
+	 * メールの添付ファイルを追加します。
+	 *
+	 * @params string $data メールのファイルデータ
+	 * @params string $mimeType メールのファイルデータのMIMEタイプ
+	 * @params string $encoding メールのファイルデータのエンコード
+     * @access public
+     */
+	public function addFile($data, $mimeType = "text/plain", $encoding = "7bit"){
+		$this->files[] = array("data" => $data, "mimeType" => $mimeType, "encoding" => $encoding);
+	}
+	
+    /**
+	 * メールのインライン画像ファイルを追加します。
+	 *
+	 * @params string $data メールのファイルデータ
+	 * @params string $mimeType メールのファイルデータのMIMEタイプ
+	 * @params string $encoding メールのファイルデータのエンコード
+     * @access public
+     */
+	public function addInlineImage($data, $mimeType = "image/jpeg"){
+		$this->images[] = array("data" => $data, "mimeType" => $mimeType);
+	}
 	
     /**
 	 * テキストメールを送信します。
@@ -116,54 +169,122 @@ class SendMail{
      * @access public
      */
 	public function send($contentType = "text/plain", $suffix = ""){
-		// メールヘッダを作成
-		$this->sendRaw($this->from, $this->fromAddress, $this->to, $this->subject, $contentType."; charset=iso-2022-jp", mb_convert_encoding($this->body, "JIS", "UTF-8"));
-		
-		try{
-			// ローダーを初期化
-			$loader = new PluginLoader();
-			$loader->LoadSetting();
-			// メールログのテーブルモデルを読み込み
-			$maillogs = $loader->LoadTable("MaillogsTable");
-					
-			// データベースINSERTモデルの読み込み
-			$insert = new DatabaseInsert($maillogs);
-			
-			// 設定するデータ配列を定義
-			$values = array();
-			$values["mail_from"] = $this->fromAddress;
-			$values["mail_to"] = $this->toAddress;
-			$values["subject"] = $this->subject;
-			$values["body"] = $this->body;
-			$values["mail_time"] = date("Y-m-d H:i:s");
-			
-			// INSERTの実行
-			$insert->execute($values);
-		}catch(Exception $e){
-			// メールログの書き込み失敗はエラーと見なさない。
+		// 添付ファイルが存在している場合にはマルチパートメールにする。
+		if(!empty($this->extBody) || count($this->images) > 0 || count($this->files) > 0){
+			$boundary_base = "claymail-" . uniqid("b");
+			$boundary = $boundary_base."_mixed";
+			$boundary2 = $boundary_base."_related";
+			$boundary3 = $boundary_base."_alternative";
+			$body = "\n--".$boundary."\n";
+			$body .= "Content-Type: multipart/related;boundary=\"".$boundary2."\"\n\n";
+			$body .= "\n--".$boundary2."\n";
+			$body .= "Content-Type: multipart/alternative;boundary=\"".$boundary3."\"\n\n";
+			$body .= "\n--".$boundary3."\n";
+			$body .= "Content-Type: ".$contentType."; charset=iso-2022-jp\n";
+			$body .= "Content-Transfer-Encoding: quoted-printable\n\n";
+			$body .= $this->qp_encode(mb_convert_encoding($this->body.$suffix, "JIS", "UTF-8"));
+			if(!empty($this->extBody)){
+				$body .= "\n--".$boundary3."\n";
+				$body .= "Content-Type: ".$this->extBody["mimeType"]."\n";
+				$body .= "Content-Transfer-Encoding: ".$this->extBody["encoding"]."\n\n";
+				if($this->extBody["encoding"] == "quoted-printable"){
+					$body .= $this->qp_encode($this->extBody["data"]);
+				}elseif($this->extBody["encoding"] == "base64"){
+					$body .= chunk_split(base64_encode($this->extBody["data"]));
+				}else{
+					$body .= $this->extBody["data"];
+				}
+			}
+			$body .= "\n--".$boundary3."--\n";
+			if(count($this->images) > 0){
+				foreach($this->files as $file){
+					$body .= "\n--".$boundary2."\n";
+					$body .= "Content-Type: ".$file["mimeType"]."\n";
+					$body .= "Content-Transfer-Encoding: base64\n\n";
+					$body .= chunk_split(base64_encode($file["data"]));
+				}
+			}
+			$body .= "\n--".$boundary2."--\n";
+			if(count($this->files) > 0){
+				foreach($this->files as $file){
+					$body .= "\n--".$boundary."\n";
+					$body .= "Content-Type: ".$file["mimeType"]."\n";
+					$body .= "Content-Transfer-Encoding: ".$file["encoding"]."\n\n";
+					if($file["encoding"] == "quoted-printable"){
+						$body .= $this->qp_encode($file["data"]);
+					}elseif($file["encoding"] == "base64"){
+						$body .= chunk_split(base64_encode($file["data"]));
+					}else{
+						$body .= $file["data"];
+					}
+				}
+			}
+			$body .= "\n--".$boundary."--\n";
+			$contentType = "multipart/mixed;boundary=\"".$boundary."\"";
+			$transferEncoding = "";
+		}else{
+			// デフォルトのコンテンツタイプと本文を設定
+			$contentType = $contentType."; charset=iso-2022-jp";
+			$transferEncoding = "7bit";
+			$body = mb_convert_encoding($this->body.$suffix, "JIS", "UTF-8");
 		}
+		// メールヘッダを作成
+		$this->sendRaw($this->from, $this->fromAddress, $this->to, $this->subject, $body, $contentType, $transferEncoding);
 	}
-	
+
     /**
-	 * テキストメールを返信します。
-	 * 送信時、送信元と送信先が入れ替わります。
+	 * テキストメールを送信します。
 	 *
 	 * @parmas string $contentType メール全体のコンテンツタイプ。
 	 * @params string $suffix メールの最後に付加する文字列メール毎に違う文章を設定する際に利用。
      * @access public
      */
-	public function reply($contentType = "text/plain", $suffix = ""){
-		$this->sendRaw($this->to, $this->toAddress, $this->from, $this->subject, $contentType."; charset=iso-2022-jp", mb_convert_encoding($this->body, "JIS", "UTF-8"));
+	public function sendlog($contentType = "text/plain", $suffix = ""){
+		$this->send($contentType, $suffix);
+		
+		// メールログに書き込み
+		$this->logMail();
 	}
 	
-	public function sendRaw($from, $fromAddress, $to, $subject, $contentType, $body){
+	/**
+	 * メールログに保存する。
+	 */
+	protected function logMail(){
+		// ローダーを初期化
+		$loader = new PluginLoader();
+		$loader->LoadSetting();
+		// メールログのテーブルモデルを読み込み
+		$maillogs = $loader->LoadTable("MaillogsTable");
+				
+		// データベースINSERTモデルの読み込み
+		$insert = new DatabaseInsert($maillogs);
+		
+		// 設定するデータ配列を定義
+		$values = array();
+		$values["mail_from"] = $this->fromAddress;
+		$values["mail_to"] = $this->toAddress;
+		$values["subject"] = $this->subject;
+		$values["body"] = $this->body;
+		$values["mail_time"] = date("Y-m-d H:i:s");
+		
+		// INSERTの実行
+		try{
+			$insert->execute($values);
+		}catch(Exception $e){
+			// メールログの書き込み失敗はエラーと見なさない。
+		}
+	} 
+	
+	public function sendRaw($from, $fromAddress, $to, $subject, $body, $contentType = "text/plain", $transferEncoding = "7bit"){
 		// メールヘッダを作成
 		$header = "";
 		$header .= "From: ".$from."\n";
 		$header .= "Reply-To: ".$from."\n";
 		$header .= "MIME-Version: 1.0\n";
 		$header .= "Content-Type: ".$contentType."\n";
-		$header .= "Content-Transfer-Encoding: 7bit\n";
+		if(!empty($transferEncoding)){
+			$header .= "Content-Transfer-Encoding: ".$transferEncoding."\n";
+		}
 		$header .= "X-Mailer: PHP/".phpversion();
 		
 		if(!mail($to, $subject, $body, $header, "-f ".$fromAddress)){
@@ -210,358 +331,6 @@ class SendMail{
 				return trim ( $out );
 			}
 		}
-	}
-}
-
-/**
- * HTML形式のメール送信に使用するクラスです。
- *
- * @package Common
- * @author Naohisa Minagawa <info@sweetberry.jp>
- */
-class SendPCHtmlMail extends SendMail{
-	/**
-	 * プレフィックス１
-	 * これはマルチパートのエンベロープ部分の為に使用します。
-	 */
-	private $prefix;
-	
-	/**
-	 * HTMLメールを読めないクライアントのための代替テキスト
-	 */
-	private $extBody;
-
-	/**
-	 * マルチパート用区切り文字列
-	 * multipart/alternativeに使用します。
-	 */
-	private $boundary;
-
-    /**
-	 * コンストラクタです。HTMLメールのための初期設定を行います。
-	 *
-     * @access public
-     */
-	public function __construct(){
-		parent::__construct();
-		
-		$this->extBody = "";
-		
-		// マルチパートのバウンダリを設定
-		$this->boundary = "MINES-" . uniqid("b");
-
-		// Bodyのヘッダ部分を設定
-		$this->prefix = "\n--".$this->boundary."\n";
-		$this->prefix .= "Content-Type: text/html; charset=\"iso-2022-jp\"\n";
-		$this->prefix .= "Content-Transfer-Encoding: quoted-printable\n\n";
-	}
-	
-    /**
-	 * HTMLメールが読めないクライアントのための代替テキストを設定します。
-	 *
-	 * @params string $parts 代替テキストとして追加指定する文字列
-     * @access public
-     */
-	public function addExtBody($parts){
-		$this->extBody .= $parts;
-	}
-	
-    /**
-	 * HTMLメールを送信します。
-	 *
-	 * @parmas string $contentType メール全体のコンテンツタイプ。マルチパートで送信するこのクラスでは原則使用しない。
-	 * @params string $suffix メールの最後に付加する文字列メール毎に違う文章を設定する際に利用。
-     * @access public
-     */
-	public function send($contentType = "", $suffix = ""){
-		// コンテンツタイプを設定
-		if(empty($contentType)){
-			$contentType = "multipart/alternative;boundary=\"".$this->boundary."\"";
-		}
-		
-		// 代替テキストを設定
-		$extBodyHead = "\n--".$this->boundary."\n";
-		$extBodyHead .= "Content-Type: text/plain; charset=\"iso-2022-jp\"\n";
-		$extBodyHead .= "Content-Transfer-Encoding: 7bit\n\n";
-		if(empty($this->extBody)){
-			$extBody = preg_replace(array("/<(div|br)\\s*\\/?>/i", "/<[^>]+>/"), array("\r\n", ""), $this->body);
-		}else{
-			$extBody = $this->extBody;
-		}
-		$suffix = "\n--".$this->boundary."--\n";
-				
-		// メールヘッダを作成
-		$this->sendRaw($this->from, $this->fromAddress, $this->to, $this->subject, $contentType, $extBodyHead.trim(mb_convert_encoding($extBody, "JIS", "UTF-8")).$this->prefix.$this->qp_encode(mb_convert_encoding($this->body, "JIS", "UTF-8")).$suffix);
-		
-		try{
-			// ローダーを初期化
-			$loader = new PluginLoader();
-			$loader->LoadSetting();
-			// メールログのテーブルモデルを読み込み
-			$maillogs = $loader->LoadTable("MaillogsTable");
-					
-			// データベースINSERTモデルの読み込み
-			$insert = new DatabaseInsert($maillogs, $this->db);
-			
-			// 設定するデータ配列を定義
-			$values = array();
-			$values["mail_from"] = $this->fromAddress;
-			$values["mail_to"] = $this->toAddress;
-			$values["subject"] = $this->subject;
-			$values["body"] = $this->prefix.$this->body.$suffix;
-			$values["mail_time"] = date("Y-m-d H:i:s");
-			
-			// INSERTの実行
-			$insert->execute($values);
-		}catch(Exception $e){
-			// メールログの書き込み失敗はエラーと見なさない。
-		}
-	}
-	
-    /**
-	 * HTMLメールを返信します。
-	 * 送信時、送信元と送信先が入れ替わります。
-	 *
-	 * @parmas string $contentType メール全体のコンテンツタイプ。マルチパートで送信するこのクラスでは原則使用しない。
-	 * @params string $suffix メールの最後に付加する文字列メール毎に違う文章を設定する際に利用。
-     * @access public
-     */
-	public function reply($contentType = "", $suffix = ""){
-		// コンテンツタイプを設定
-		if(empty($contentType)){
-			$contentType = "multipart/alternative;boundary=\"".$this->boundary."\"";
-		}
-		
-		// 代替テキストを設定
-		$extBodyHead = "\n--".$this->boundary."\n";
-		$extBodyHead .= "Content-Type: text/plain; charset=\"iso-2022-jp\"\n";
-		$extBodyHead .= "Content-Transfer-Encoding: 7bit\n\n";
-		if(empty($this->extBody)){
-			$extBody = preg_replace(array("/<(div|br)\\s*\\/?>/i", "/<[^>]+>/"), array("\r\n", ""), $this->body);
-		}else{
-			$extBody = $this->extBody;
-		}
-		$suffix = "\n--".$this->boundary."--\n";
-				
-		// メールヘッダを作成
-		$this->sendRaw($this->to, $this->toAddress, $this->from, $this->subject, $contentType, $extBodyHead.trim(mb_convert_encoding($extBody, "JIS", "UTF-8")).$this->prefix.$this->qp_encode(mb_convert_encoding($this->body, "JIS", "UTF-8")).$suffix);
-	}
-}
-
-/**
- * HTML形式のメール送信に使用するクラスです。
- *
- * @package Common
- * @author Naohisa Minagawa <info@sweetberry.jp>
- */
-class SendHtmlMail extends SendMail{
-	/**
-	 * プレフィックス１
-	 * これはマルチパートのエンベロープ部分の為に使用します。
-	 */
-	private $prefix;
-	
-	/**
-	 * プレフィックス２
-	 * これはHTMLパートのヘッダ部分の為に使用します。
-	 */
-	private $prefix2;
-	
-	/**
-	 * HTMLメールを読めないクライアントのための代替テキスト
-	 */
-	private $extBody;
-
-	/**
-	 * マルチパート用区切り文字列
-	 * multipart/mixedに使用します。
-	 */
-	private $boundary1;
-	
-	/**
-	 * マルチパート用区切り文字列
-	 * multipart/relatedに使用します。
-	 */
-	private $boundary2;
-	
-	/**
-	 * マルチパート用区切り文字列
-	 * multipart/alternativeに使用します。
-	 */
-	private $boundary3;
-
-	/**
-	 * 画像識別用キーに指定するサフィックス
-	 */
-	private $imageKey;
-
-	/**
-	 * 画像データの配列
-	 */
-	private $images;
-	
-    /**
-	 * コンストラクタです。HTMLメールのための初期設定を行います。
-	 *
-     * @access public
-     */
-	public function __construct(){
-		parent::__construct();
-		
-		$this->extBody = "";
-		
-		$this->images = array();
-		$this->imageKey = date("YmdHis");
-
-		// マルチパートのバウンダリを設定
-		$this->boundary1 = "MINES-" . uniqid("b");
-		$this->boundary2 = "MINES-" . uniqid("b")."_01";
-		$this->boundary3 = "MINES-" . uniqid("b")."_02";
-
-		// Bodyのヘッダ部分を設定
-		$this->prefix = "\n--".$this->boundary1."\n";
-		$this->prefix .= "Content-Type: multipart/related;boundary=\"".$this->boundary2."\"\n";
-		$this->prefix .= "\n--".$this->boundary2."\n";
-		$this->prefix .= "Content-Type: multipart/alternative;boundary=\"".$this->boundary3."\"\n";
-		$this->prefix2 = "\n--".$this->boundary3."\n";
-		$this->prefix2 .= "Content-Type: text/html; charset=\"iso-2022-jp\"\n";
-		$this->prefix2 .= "Content-Transfer-Encoding: quoted-printable\n\n";
-	}
-	
-    /**
-	 * HTMLメールが読めないクライアントのための代替テキストを設定します。
-	 *
-	 * @params string $parts 代替テキストとして追加指定する文字列
-     * @access public
-     */
-	public function addExtBody($parts){
-		$this->extBody .= $parts;
-	}
-	
-    /**
-	 * HTMLメールに画像リンクを追加します。
-	 *
-	 * @params binary $data 画像データ
-     * @access public
-     */
-	public function addImage($data){
-		$this->images[] = $data;
-		$this->addBody("<IMG src=\"cid:".sprintf("%02d", count($this->images))."@".$this->imageKey."\">");
-	}
-	
-    /**
-	 * HTMLメールを送信します。
-	 *
-	 * @parmas string $contentType メール全体のコンテンツタイプ。マルチパートで送信するこのクラスでは原則使用しない。
-	 * @params string $suffix メールの最後に付加する文字列メール毎に違う文章を設定する際に利用。
-     * @access public
-     */
-	public function send($contentType = "", $suffix = ""){
-		// コンテンツタイプを設定
-		if(empty($contentType)){
-			$contentType = "multipart/mixed;boundary=\"".$this->boundary1."\"";
-		}
-		
-		// 代替テキストを設定
-		$extBodyHead = "\n--".$this->boundary3."\n";
-		$extBodyHead .= "Content-Type: text/plain; charset=\"iso-2022-jp\"\n";
-		$extBodyHead .= "Content-Transfer-Encoding: 7bit\n\n";
-		if(empty($this->extBody)){
-			$extBody = preg_replace(array("/<(div|br)\\s*\\/?>/i", "/<[^>]+>/"), array("\r\n", ""), $this->body);
-		}else{
-			$extBody = $this->extBody;
-		}
-		
-		// 添付画像を設定
-		$suffix .= 	"\n--".$this->boundary3."--\n";
-		foreach($this->images as $index => $imageData){
-			$suffix .= "\n--".$this->boundary2."\n";
-			if ( preg_match( '/^\x89PNG\x0d\x0a\x1a\x0a/', $imageData) )  {
-				$suffix .= "Content-Type: image/png; name=\"image.png\"\n";
-			} elseif ( preg_match( '/^GIF8[79]a/', $imageData) )  {
-				$suffix .= "Content-Type: image/gif; name=\"image.gif\"\n";
-			} elseif ( preg_match( '/^\xff\xd8/', $imageData) )  {
-				$suffix .= "Content-Type: image/jpeg; name=\"image.jpg\"\n";
-			}
-			$suffix .= "Content-Transfer-Encoding: base64\n";
-			$suffix .= "Content-ID: <".sprintf("%02d", ($index + 1))."@".$this->imageKey.">\n\n";
-			$suffix .= chunk_split(base64_encode($imageData));
-			$suffix .= "\n--".$this->boundary2."--\n";
-		}
-		$suffix .= "\n--".$this->boundary1."--\n";
-		
-		// メールヘッダを作成
-		$this->sendRaw($this->from, $this->fromAddress, $this->to, $this->subject, $contentType, $this->prefix.$extBodyHead.trim(mb_convert_encoding($extBody, "JIS", "UTF-8")).$this->prefix2.$this->qp_encode(mb_convert_encoding($this->body, "JIS", "UTF-8")).$suffix);
-		
-		try{
-			// ローダーを初期化
-			$loader = new PluginLoader();
-			$loader->LoadSetting();
-			// メールログのテーブルモデルを読み込み
-			$maillogs = $loader->LoadTable("MaillogsTable");
-					
-			// データベースINSERTモデルの読み込み
-			$insert = new DatabaseInsert($maillogs, $this->db);
-			
-			// 設定するデータ配列を定義
-			$values = array();
-			$values["mail_from"] = $this->fromAddress;
-			$values["mail_to"] = $this->toAddress;
-			$values["subject"] = $this->subject;
-			$values["body"] = $this->prefix.$this->body.$suffix;
-			$values["mail_time"] = date("Y-m-d H:i:s");
-			
-			// INSERTの実行
-			$insert->execute($values);
-		}catch(Exception $e){
-			// メールログの書き込み失敗はエラーと見なさない。
-		}
-	}
-	
-    /**
-	 * HTMLメールを返信します。
-	 * 送信時、送信元と送信先が入れ替わります。
-	 *
-	 * @parmas string $contentType メール全体のコンテンツタイプ。マルチパートで送信するこのクラスでは原則使用しない。
-	 * @params string $suffix メールの最後に付加する文字列メール毎に違う文章を設定する際に利用。
-     * @access public
-     */
-	public function reply($contentType = "", $suffix = ""){
-		// コンテンツタイプを設定
-		if(empty($contentType)){
-			$contentType = "multipart/mixed;boundary=\"".$this->boundary1."\"";
-		}
-		
-		// 代替テキストを設定
-		$extBody = "\n--".$this->boundary3."\n";
-		$extBody .= "Content-Type: text/plain; charset=\"iso-2022-jp\"\n";
-		$extBody .= "Content-Transfer-Encoding: 7bit\n\n";
-		if(empty($this->extBody)){
-			$extBody = preg_replace("/<[^>]>/", "", $this->body);
-		}else{
-			$extBody = $this->extBody;
-		}
-		
-		// 添付画像を設定
-		$suffix .= 	"\n--".$this->boundary3."--\n";
-		foreach($this->images as $index => $imageData){
-			$suffix .= "\n--".$this->boundary2."\n";
-			if ( preg_match( '/^\x89PNG\x0d\x0a\x1a\x0a/', $imageData) )  {
-				$suffix .= "Content-Type: image/png; name=\"image.png\"\n";
-			} elseif ( preg_match( '/^GIF8[79]a/', $imageData) )  {
-				$suffix .= "Content-Type: image/gif; name=\"image.gif\"\n";
-			} elseif ( preg_match( '/^\xff\xd8/', $imageData) )  {
-				$suffix .= "Content-Type: image/jpeg; name=\"image.jpg\"\n";
-			}
-			$suffix .= "Content-Transfer-Encoding: base64\n";
-			$suffix .= "Content-ID: <".sprintf("%02d", ($index + 1))."@".$this->imageKey.">\n\n";
-			$suffix .= chunk_split(base64_encode($imageData));
-			$suffix .= "\n--".$this->boundary2."--\n";
-		}
-		$suffix .= "\n--".$this->boundary1."--\n";
-		
-		// メールヘッダを作成
-		$this->sendRaw($this->to, $this->toAddress, $this->from, $this->subject, $contentType, $this->prefix.mb_convert_encoding($extBody, "JIS", "UTF-8").$this->prefix2.$this->qp_encode(mb_convert_encoding($this->body, "JIS", "UTF-8")).$suffix);
 	}
 }
 ?>
