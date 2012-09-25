b4b90b8c6bc1992154c8686a1adda2f39f14e595
Naohisa Minagawa <info@clay-system.jp>
2012-09-25 10:56:20 +0900

    一覧表示用、詳細表示用のモジュールクラスの処理を修正

--- libs/common/classes/FrameworkModule.php
+++ libs/common/classes/FrameworkModule.php
@@ -80,10 +80,8 @@ abstract class FrameworkListModule extends FrameworkModule{
 		}
 		
 		// HTTPのリクエストデータから検索条件を抽出
-		foreach($keys as $key){
-			if(isset($_POST["search"][$key])){
-				$condition[$this->getColumnByKey($key)] = $_POST["search"][$key];
-			}
+		foreach($_POST["search"] as $key => $value){
+			$condition[$this->getColumnByKey($key)] = $value;
 		}
 		
 		// 並べ替え順序が指定されている場合に適用
@@ -115,6 +113,69 @@ abstract class FrameworkListModule extends FrameworkModule{
  * ページング一覧表示用のモジュールです。
  */
 abstract class FrameworkPageModule extends FrameworkListModule{
+	public function execute($params){
+		// 検索に使用するプラグインローダーを取得する。
+		$loader = $this->getLoader();
+		$loader->LoadSetting();
+
+		// ページャの初期化
+		$pager = new TemplatePager($params->get("_pager_mode", TemplatePager::PAGE_SLIDE), $params->get("_pager_dispmode", TemplatePager::DISPLAY_ATTR), $params->get("_pager_per_page", 20), $params->get("_pager_displays", 3));
+		$pager->importTemplates($params);
+		
+		// 検索条件を構築する。
+		$conditions = array();
+		
+		// 拡張の検索条件を設定する。
+		$condition = $this->getExtendedCondition($condition, $params);
+		
+		// タグのパラメータから検索条件を抽出
+		$keys = $this->getSearchKeys();
+		foreach($keys as $key){
+			if($this->getDefaultByKey($key) != null || $params->check($key)){
+				$condition[$this->getColumnByKey($key)] = $params->get($key, $this->getDefaultByKey($key));
+			}
+		}
+		
+		// HTTPのリクエストデータから検索条件を抽出
+		foreach($keys as $key){
+			if(isset($_POST["search"][$key])){
+				$condition[$this->getColumnByKey($key)] = $_POST["search"][$key];
+			}
+		}
+		
+		// 並べ替え順序が指定されている場合に適用
+		$sortOrder = "";
+		$sortReverse = false;
+		if($params->check("_sort_key")){
+			$sortOrder = $params->get("_sort_key");
+		}
+		if(isset($_POST["_sort_key"])){
+			$sortOrder = $_POST["_sort_key"];
+		}
+		if(empty($sortOrder)){
+			$sortOrder = "create_time";
+			$sortReverse = true;
+		}elseif(preg_match("/^rev@/", $sortOrder) > 0){
+			list($dummy, $sortOrder) = explode("@", $sortOrder);
+			$sortReverse = true;
+		}
+		
+		// 商品データを検索する。
+		$model = $loader->LoadModel($this->getModelName());
+		$pager->setDataSize($model->countBy($conditions));
+		$model->limit($pager->getPageSize(), $pager->getCurrentFirstOffset());
+		$result = $model->findAllBy($conditions, $sortOrder, $sortReverse);
+		
+		$_SERVER["ATTRIBUTES"][$params->get("_result", $this->getDefaultResultKey())."_pager"] = $pager;
+		$_SERVER["ATTRIBUTES"][$params->get("_result", $this->getDefaultResultKey())] = $result;
+	}
+}
+
+
+/**
+ * ページング一覧表示用のモジュールです。
+ */
+abstract class FrameworkDetailModule extends FrameworkListModule{
 	public function execute($params){
 		// 検索に使用するプラグインローダーを取得する。
 		$loader = $this->getLoader();
